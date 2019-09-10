ae6512dc0c869a7350880e5f52f2b2445d57b281
kamalhappysanz <kamal.happysanz@gmail.com>
2019-09-10 15:29:12 +0530

    Update Class_manage.php

--- application/models/Class_manage.php
+++ application/models/Class_manage.php
@@ -94,13 +94,13 @@ Class Class_manage extends CI_Model
 			 return $resultset->result();
         }
 
-       function getall_active_class(){
-         $query="SELECT c.class_name,s.sec_name,cm.class_sec_id,cm.status FROM edu_class AS c,edu_sections AS s ,edu_classmaster AS cm WHERE cm.class = c.class_id AND cm.section = s.sec_id AND cm.status='Deactive' ORDER BY c.class_name";
+       function getall_class(){
+         $query="SELECT c.class_name,s.sec_name,cm.class_sec_id,cm.status FROM edu_class AS c,edu_sections AS s ,edu_classmaster AS cm WHERE cm.class = c.class_id AND cm.section = s.sec_id AND cm.status='Active' ORDER BY c.class_name";
          $result=$this->db->query($query);
          return $result->result();
        }
 
-       function getall__active_class(){
+       function getall_active_class(){
          $query="SELECT c.class_name,s.sec_name,cm.class_sec_id,cm.status FROM edu_class AS c,edu_sections AS s ,edu_classmaster AS cm WHERE cm.class = c.class_id AND cm.section = s.sec_id ORDER BY c.class_name";
          $result=$this->db->query($query);
          return $result->result();
