76c2818df569d3265ac60acb0eae725a4b9a2c66
Maran <maran.happysanz@gmail.com>
2019-09-20 13:01:48 +0530

    group notification
    group notification

--- application/models/Notificationmodel.php
+++ application/models/Notificationmodel.php
@@ -25,63 +25,77 @@ Class Notificationmodel extends CI_Model
       }
     }
 
-
-
-
-      function send_circular_via_push_notification()
-      {
-
-			require_once 'assets/notification/Firebase.php';
-			require_once 'assets/notification/Push.php';
-
-			$subject = "Subject";
-			$cnotes = "Notes";
-			$img_url = "";
-
-			$gcm_key = "dSZQL9myJ_4:APA91bGnN1VbtpEMv18_DsQFRHmQqUcLY_ogWVlf4tESgOkqQDxjQrdpehsZhqO5k-kj9Hqy_uCqL2Jmp_CeXh713hAI0CUNpFosTd7VKVbrcKHURvkClSPLWZ9QHXawnQaDuMGZ8SgI";
-			//$device_token = explode(",", $gcm_key);
-			$push = null;
-
-//        //first check if the push has an image with it
-			$push = new Push(
-					$subject,
-					$cnotes,
-					$img_url
+	function sendNotification($gcm_key,$notes)
+    {
+		  $gcm_key = array($gcm_key);
+		  $data = array
+				(
+				'message' 	=> $notes,
+				'vibrate'	=> 1,
+				'sound'		=> 1
 				);
 
-			//getting the push from push object
-			$mPushNotification = $push->getPush();
+            // Insert real GCM API key from the Google APIs Console
+            $apiKey = 'AAAADRDlvEI:APA91bFi-gSDCTCnCRv1kfRd8AmWu0jUkeBQ0UfILrUq1-asMkBSMlwamN6iGtEQs72no-g6Nw0lO5h4bpN0q7JCQkuTYsdPnM1yfilwxYcKerhsThCwt10cQUMKrBrQM2B3U3QaYbWQ';
+            // Set POST request body
+            $post = array(
+                'registration_ids'  => $gcm_key,
+                'data'              => $data,
+                 );
+            // Set CURL request headers
+            $headers = array(
+                'Authorization: key=' . $apiKey,
+                'Content-Type: application/json'
+                  );
+            // Initialize curl handle
+            $ch = curl_init();
+            // Set URL to GCM push endpoint
+            curl_setopt($ch, CURLOPT_URL, 'https://gcm-http.googleapis.com/gcm/send');
+            // Set request method to POST
+            curl_setopt($ch, CURLOPT_POST, true);
+            // Set custom request headers
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            // Get the response back as string instead of printing it
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            // Set JSON post data
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post));
+            // Actually send the request
+            $result = curl_exec($ch);
+			//if(!$result){ echo "Success";}
 
-			//creating firebase class object
-			$firebase = new Firebase();
-			$firebase->send($gcm_key,$mPushNotification);
+            // Handle errors
+            if (curl_errno($ch)) {
+            //echo 'GCM error: ' . curl_error($ch);
+            }
+            // Close curl handle
+            curl_close($ch);
 
-			/* foreach($device_token as $token) {
-				 $firebase->send(array($token),$mPushNotification);
-			} */
-			echo "Sucess";
-
+            // Debug GCM response
       }
+			
 
 
-
+      
      function send_notification_for_teacher_substitution($tname,$sub_teacher,$sub_tname,$leave_date,$cls_id,$period_id)
 	 {
         $sql="SELECT user_id,name,user_master_id,teacher_id FROM edu_users WHERE teacher_id='$sub_teacher' AND user_type=2 AND user_master_id='$sub_teacher'";
 		$resultset=$this->db->query($sql);
 		$res=$resultset->result();
-		foreach($res as $rows){}
-		$userid=$rows->user_id;
-		//echo $userid; exit;
-		$sql1="SELECT cm.class_sec_id,cm.class,cm.section,c.class_id,c.class_name,s.sec_id,s.sec_name FROM edu_classmaster AS cm,edu_class AS c,edu_sections AS s WHERE cm.class_sec_id='$cls_id' AND cm.class=c.class_id AND cm.section=s.sec_id ";
+		
+		foreach($res as $rows){
+			$userid=$rows->user_id;
+		}
+		
+		$sql1 = "SELECT cm.class_sec_id,cm.class,cm.section,c.class_id,c.class_name,s.sec_id,s.sec_name FROM edu_classmaster AS cm,edu_class AS c,edu_sections AS s WHERE cm.class_sec_id='$cls_id' AND cm.class=c.class_id AND cm.section=s.sec_id ";
 		$resultset1=$this->db->query($sql1);
 		$res1=$resultset1->result();
-		foreach($res1 as $cls){}
-		$cname=$cls->class_name;
-		$sename=$cls->sec_name;
-
+		
+		foreach($res1 as $cls){
+			$cname=$cls->class_name;
+			$sename=$cls->sec_name;
+		}
+		
 		$textmessage='This is to inform you that as '.$tname.' is on leave,'.$sub_tname.' will be the substitute teacher to fill in for '.$cname.'-'.$sename.',period ('.$period_id.') on '.$leave_date.' ';
-
 		$data=array(
 				  'message' => $textmessage,
 				  'ctitle'  => "SUBSTITUTION",
@@ -92,6 +106,7 @@ Class Notificationmodel extends CI_Model
 		$sql="SELECT * FROM edu_notification WHERE user_id='$userid'";
 		$tgsm=$this->db->query($sql);
 		$tres1=$tgsm->result();
+		
 		foreach($tres1 as $trow)
 		{
 		   $gsmkey=array($trow->gcm_key);
@@ -127,7 +142,7 @@ Class Notificationmodel extends CI_Model
 
        
 	function send_circular_via_notification($title_id,$notes,$tusers_id,$stusers_id,$pusers_id,$bmusers_id,$users_id)
-         {
+     {
 			$ssql = "SELECT * FROM edu_circular_master WHERE id ='$title_id'";
 			$res = $this->db->query($ssql);
 			$result =$res->result();
@@ -471,7 +486,7 @@ Class Notificationmodel extends CI_Model
 										if (!$fp)
 											exit("Failed to connect: $err $errstr" . PHP_EOL);
 																		
-											$msg = chr(0) . pack("n", 32) . pack("H*", str_replace(" ", "", $token)) . pack("n", strlen($payload)) . $payload;
+											$msg = chr(0) . pack("n", 32) . pack("H*", str_replace(" ", "", array($gcm_key))) . pack("n", strlen($payload)) . $payload;
 											$result = fwrite($fp, $msg, strlen($msg));
 											fclose($fp);
 										
@@ -510,7 +525,7 @@ Class Notificationmodel extends CI_Model
 
 									//creating firebase class object
 									$firebase = new Firebase();
-									$firebase->send($gcm_key,$mPushNotification);		
+									$firebase->send(array($gcm_key),$mPushNotification);		
 									
 								} else {
 									
@@ -525,7 +540,7 @@ Class Notificationmodel extends CI_Model
 									if (!$fp)
 										exit("Failed to connect: $err $errstr" . PHP_EOL);
 																			
-										$msg = chr(0) . pack("n", 32) . pack("H*", str_replace(" ", "", $token)) . pack("n", strlen($payload)) . $payload;
+										$msg = chr(0) . pack("n", 32) . pack("H*", str_replace(" ", "", array($gcm_key))) . pack("n", strlen($payload)) . $payload;
 										$result = fwrite($fp, $msg, strlen($msg));
 										fclose($fp);
 									
@@ -564,7 +579,7 @@ Class Notificationmodel extends CI_Model
 
 									//creating firebase class object
 									$firebase = new Firebase();
-									$firebase->send($gcm_key,$mPushNotification);		
+									$firebase->send(array($gcm_key),$mPushNotification);		
 									
 								} else {
 									
@@ -580,7 +595,7 @@ Class Notificationmodel extends CI_Model
 										exit("Failed to connect: $err $errstr" . PHP_EOL);
 									
 										
-										$msg = chr(0) . pack("n", 32) . pack("H*", str_replace(" ", "", $token)) . pack("n", strlen($payload)) . $payload;
+										$msg = chr(0) . pack("n", 32) . pack("H*", str_replace(" ", "", array($gcm_key))) . pack("n", strlen($payload)) . $payload;
 										$result = fwrite($fp, $msg, strlen($msg));
 										fclose($fp);
 									
@@ -596,114 +611,173 @@ Class Notificationmodel extends CI_Model
 		 }
 	   
 
-
-
-      function sendNotification($gcm_key,$notes)
-            {
-              $gcm_key = array($gcm_key);
-              $data = array
-                    (
-                    'message' 	=> $notes,
-                    'vibrate'	=> 1,
-                    'sound'		=> 1
-
-                    );
-
-            // Insert real GCM API key from the Google APIs Console
-            $apiKey = 'AAAADRDlvEI:APA91bFi-gSDCTCnCRv1kfRd8AmWu0jUkeBQ0UfILrUq1-asMkBSMlwamN6iGtEQs72no-g6Nw0lO5h4bpN0q7JCQkuTYsdPnM1yfilwxYcKerhsThCwt10cQUMKrBrQM2B3U3QaYbWQ';
-            // Set POST request body
-            $post = array(
-                'registration_ids'  => $gcm_key,
-                'data'              => $data,
-                 );
-            // Set CURL request headers
-            $headers = array(
-                'Authorization: key=' . $apiKey,
-                'Content-Type: application/json'
-                  );
-            // Initialize curl handle
-            $ch = curl_init();
-            // Set URL to GCM push endpoint
-            curl_setopt($ch, CURLOPT_URL, 'https://gcm-http.googleapis.com/gcm/send');
-            // Set request method to POST
-            curl_setopt($ch, CURLOPT_POST, true);
-            // Set custom request headers
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-            // Get the response back as string instead of printing it
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            // Set JSON post data
-            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post));
-            // Actually send the request
-            $result = curl_exec($ch);
-			//if(!$result){ echo "Success";}
-
-            // Handle errors
-            if (curl_errno($ch)) {
-            //echo 'GCM error: ' . curl_error($ch);
-            }
-            // Close curl handle
-            curl_close($ch);
-
-            // Debug GCM response
-
-            }
-
-
-            //Group Notification
-         function send_notification($group_id,$notes,$user_id,$members_id){
+//Group Notification
+       function send_notification($group_id,$notes,$user_id,$members_id)
+		{
+			
+			require_once 'assets/notification/Firebase.php';
+			require_once 'assets/notification/Push.php';
+			$title = 'Group Notification';
+			$push = null;
+			 //first check if the push has an image with it
+			$push = new Push(
+					$title,
+					$notes,
+					null
+				);
+							
+		
+			$passphrase = 'hs123';
+			$loction ='assets/notification/heylaapp.pem';
+			$payload = '{
+						"aps": {
+							"alert": {
+								"body": "'.$notes.'",
+								"title": "'.$title.'"
+							}
+						}
+					}';
+			/* $payload = '{
+						"aps": {
+							"alert": {
+								"body": "'.$notes.'",
+								"title": "'.$title.'"
+							},
+							"mutable-content": 1
+						},
+						"mediaUrl": "'.$img_url.'",
+						"mediaType": "image"
+					}'; */
+					
            $check_type="SELECT * FROM edu_grouping_members WHERE group_title_id='$group_id'";
            $get_type=$this->db->query($check_type);
            $res_type=$get_type->result();
-           foreach($res_type as $row_type){}
-           $member_type=$row_type->member_type;
-           $group_member_id=$row_type->group_member_id;
-           if($member_type='3'){
-             $class="SELECT egm.group_member_id,ep.email,ep.mobile,ep.id FROM edu_grouping_members AS egm
-             LEFT JOIN edu_users AS eu ON eu.user_id=egm.group_member_id LEFT JOIN edu_admission AS ea ON ea.admission_id=eu.user_master_id
-             LEFT JOIN edu_parents AS ep ON FIND_IN_SET(ea.admission_id, ep.admission_id) LEFT JOIN edu_notification AS en ON en.user_id=eu.user_id
-             WHERE  egm.group_title_id='$group_id' AND ep.primary_flag='yes'";
-            $pcell=$this->db->query($class);
-            $res2=$pcell->result();
-            foreach($res2 as $result){
-            $parent_id=$result->id;
-            $sql="SELECT eu.user_id,en.gcm_key FROM edu_users as eu left join edu_notification as en on eu.user_id=en.user_id WHERE user_type='4' and user_master_id='$parent_id'";
-             $sgsm=$this->db->query($sql);
-             $res=$sgsm->result();
-             foreach($res as $row){
-             $gcm_key=$row->gcm_key;
-             $this->sendNotification($gcm_key,$notes);
-            }
+           foreach($res_type as $row_type){
+			   $member_type=$row_type->member_type;
+			   $group_member_id=$row_type->group_member_id;
+		   }
+		   
+           if($member_type='3')
+		   {
+				$class="SELECT egm.group_member_id,ep.email,ep.mobile,ep.id FROM edu_grouping_members AS egm
+				LEFT JOIN edu_users AS eu ON eu.user_id=egm.group_member_id LEFT JOIN edu_admission AS ea ON ea.admission_id=eu.user_master_id
+				LEFT JOIN edu_parents AS ep ON FIND_IN_SET(ea.admission_id, ep.admission_id) LEFT JOIN edu_notification AS en ON en.user_id=eu.user_id
+				WHERE  egm.group_title_id='$group_id' AND ep.primary_flag='yes'";
+				$pcell=$this->db->query($class);
+				$res2=$pcell->result();
+				
+				foreach($res2 as $result){
+					$parent_id=$result->id;
+					$sql="SELECT eu.user_id,en.gcm_key FROM edu_users as eu left join edu_notification as en on eu.user_id=en.user_id WHERE user_type='4' and user_master_id='$parent_id'";
+					$sgsm=$this->db->query($sql);
+					$res=$sgsm->result();
+						foreach($res as $row){
+							$gcm_key=$row->gcm_key;
+							$mobile_type=$row->mobile_type;
+							
+							if ($mobile_type =='1'){
 
-          }
+									//getting the push from push object
+									$mPushNotification = $push->getPush();
+
+									//creating firebase class object
+									$firebase = new Firebase();
+									$firebase->send(array($gcm_key),$mPushNotification);		
+									
+								} else {
+									
+
+									$ctx = stream_context_create();
+									stream_context_set_option($ctx, 'ssl', 'local_cert', $loction);
+									stream_context_set_option($ctx, 'ssl', 'passphrase', $passphrase);
+
+									// Open a connection to the APNS server
+									$fp = stream_socket_client('ssl://gateway.sandbox.push.apple.com:2195', $err, $errstr, 60, STREAM_CLIENT_CONNECT|STREAM_CLIENT_PERSISTENT, $ctx);
+
+									if (!$fp)
+										exit("Failed to connect: $err $errstr" . PHP_EOL);
+									
+										
+										$msg = chr(0) . pack("n", 32) . pack("H*", str_replace(" ", "", array($gcm_key))) . pack("n", strlen($payload)) . $payload;
+										$result = fwrite($fp, $msg, strlen($msg));
+										fclose($fp);
+									
+							}
+							
+							//$this->sendNotification($gcm_key,$notes);
+						}
+
+				}
            }
+		   
            $check_type_staff="SELECT * FROM edu_grouping_members WHERE group_title_id='$group_id'";
            $get_type_staff=$this->db->query($check_type_staff);
            $res_type_staff=$get_type_staff->result();
-           foreach($res_type_staff as $row_type_staff){}
-           $member_type_staff=$row_type_staff->member_type;
-           $group_member_id_staff=$row_type_staff->group_member_id;
-            if($member_type='2' || $member_type='5'){
-              $select="SELECT * from edu_users as eu where user_master_id IN($group_member_id_staff) and eu.user_type='5' and eu.user_type='2'";
-              $resultset=$this->db->query($select);
-              $result=$resultset->result();
-              if(empty($res)){
-              }else{
-                foreach($result as $notification_id){
-                  $notify_id=$notification_id->user_id;
-                  $sql="SELECT * FROM edu_notification where user_id='$notify_id'";
-                   $sgsm=$this->db->query($sql);
-                   $res=$sgsm->result();
-                   foreach($res as $row){
-                   $gcm_key=$row->gcm_key;
-                   $this->sendNotification($gcm_key,$notes);
-                  }
+		   
+           foreach($res_type_staff as $row_type_staff){
+			   $member_type_staff=$row_type_staff->member_type;
+			   $group_member_id_staff=$row_type_staff->group_member_id;
+		   }
+		   
+            if($member_type='2' || $member_type='5')
+			{
+				  $select="SELECT * from edu_users as eu where user_master_id IN($group_member_id_staff) and eu.user_type='5' and eu.user_type='2'";
+				  $resultset=$this->db->query($select);
+				  $result=$resultset->result();
+				  if(empty($res)){
+				  }else{
+					foreach($result as $notification_id){
+						$notify_id=$notification_id->user_id;
+						$sql="SELECT * FROM edu_notification where user_id='$notify_id'";
+						$sgsm=$this->db->query($sql);
+						$res=$sgsm->result();
+					   
+					   foreach($res as $row){
+						   $gcm_key=$row->gcm_key;
+						   $mobile_type=$row->mobile_type;
+						   
+						   if ($mobile_type =='1'){
 
-                }
-              }
+									//getting the push from push object
+									$mPushNotification = $push->getPush();
+
+									//creating firebase class object
+									$firebase = new Firebase();
+									$firebase->send(array($gcm_key),$mPushNotification);		
+									
+								} else {
+									
+
+									$ctx = stream_context_create();
+									stream_context_set_option($ctx, 'ssl', 'local_cert', $loction);
+									stream_context_set_option($ctx, 'ssl', 'passphrase', $passphrase);
+
+									// Open a connection to the APNS server
+									$fp = stream_socket_client('ssl://gateway.sandbox.push.apple.com:2195', $err, $errstr, 60, STREAM_CLIENT_CONNECT|STREAM_CLIENT_PERSISTENT, $ctx);
+
+									if (!$fp)
+										exit("Failed to connect: $err $errstr" . PHP_EOL);
+									
+										
+										$msg = chr(0) . pack("n", 32) . pack("H*", str_replace(" ", "", array($gcm_key))) . pack("n", strlen($payload)) . $payload;
+										$result = fwrite($fp, $msg, strlen($msg));
+										fclose($fp);
+									
+							}
+						   
+						   //$this->sendNotification($gcm_key,$notes);
+					  }
+
+					}
+				  }
             }
-
-
       }
+
+
+
+
+
+
 
 // Home Work Details
 
