215629ef537ee110d0c283101887711035d2930e
Maran <maran.happysanz@gmail.com>
2019-09-27 11:45:30 +0530

    Update Notificationmodel.php

--- application/models/Notificationmodel.php
+++ application/models/Notificationmodel.php
@@ -208,8 +208,8 @@ Class Notificationmodel extends CI_Model
        
 	function send_circular_via_notification($title_id,$notes,$tusers_id,$stusers_id,$pusers_id,$bmusers_id,$users_id)
      {
-			echo $ssql = "SELECT * FROM edu_circular_master WHERE id ='$title_id'";
-			exit;
+			$ssql = "SELECT * FROM edu_circular_master WHERE id ='$title_id'";
+			//exit;
 			$res = $this->db->query($ssql);
 			$result =$res->result();
 			
