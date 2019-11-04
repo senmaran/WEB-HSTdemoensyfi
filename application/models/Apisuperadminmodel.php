0198a8bbf9e10559ffd33a6161d897ee44cf3360
kamalhappysanz <kamal.happysanz@gmail.com>
2019-11-04 10:55:51 +0530

    Update Apisuperadminmodel.php

--- application/models/Apisuperadminmodel.php
+++ application/models/Apisuperadminmodel.php
@@ -28,12 +28,23 @@ Class Apisuperadminmodel extends CI_Model
       $query="SELECT count(user_id) as user_count,eu.user_type,er.user_type_name from edu_users as eu left join edu_role as er on er.role_id=eu.user_type GROUP by eu.user_type";
       $res    = $this->db->query($query);
       if($res->num_rows()==0){
-        $data=array('status'=>"error");
+        $total_count=array('status'=>"error");
       }else{
         $result=$res->result();
-        $data=array('status'=>"success","user_data"=>$result);
+        $total_count=array('status'=>"success","user_data"=>$result);
       }
-      return $data;
+
+      $query_student="SELECT count(admit_year) as student,admit_year FROM edu_enrollment GROUP BY admit_year";
+      $res_student    = $this->db->query($query_student);
+      if($res_student->num_rows()==0){
+        $year_based_student=array('status'=>"error");
+      }else{
+        $result_student=$res_student->result();
+        $year_based_student=array('status'=>"success","year_stu_count"=>$result_student);
+      }
+
+      $response=array("status"=>"success","total_count"=>$total_count,"year_based_count"=>$year_based_student);
+      return $response;
     }
 
 
