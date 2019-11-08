eb4cff933bdd9215fe2576850faf80fe9a7d7e36
Maran <maran.happysanz@gmail.com>
2019-11-08 15:29:25 +0530

    forgot password
    forgot password

--- application/views/forgotpassword.php
+++ application/views/forgotpassword.php
@@ -203,7 +203,7 @@ we'll reset the password for you.</p>
 				           data: $('#forgotform').serialize(),
 				           success: function(response) {
 										// alert(response);
-										 if(response=="Password  Reset and send to your Mail Please check it"){
+										 if(response=="Password Reset and send to your Mail Please check it"){
 											 swal({
 												   title: "Success",
 												    text: response,
